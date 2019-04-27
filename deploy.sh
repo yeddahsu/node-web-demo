3fdaf04f988a75b82e1fb22b19021ca01dc8d842
yeddahsu <yeddahsu@gmail.com>
2019-04-28 01:08:48 +0800

    13th

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "Updating staging Server”
-echo "stopping node-web-demo.service”
+echo "Updating staging Server"
+echo "stopping node-web-demo.service"
 sudo systemctl stop node-web-demo.service
 # remove all outdated images and containers
-echo "removing outdated/dangling images and containers”
+echo "removing outdated/dangling images and containers"
 sudo docker rmi yedda/node-web-demo:latest
 # create new image for node-web-demo
-echo "create new image for node-web-demo”
+echo "create new image for node-web-demo"
 cd /home/yedda/node-web-demo
 git pull origin master
-sudo docker build -t=”yedda/node-web-demo” .
+sudo docker build -t="yedda/node-web-demo" .
 # restart service which will use the newly pulled image
-echo "restarting node-web-demo service”
+echo "restarting node-web-demo service"
 sudo systemctl start node-web-demo.service
