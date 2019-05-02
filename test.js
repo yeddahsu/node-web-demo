6ead7c24c23d45b007abf5a3369fea52d2464af4
yeddahsu <yeddahsu@gmail.com>
2019-05-02 18:42:52 +0800

    51st

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ describe('Test function "testMe"', function () {
             .expect((res) => {
                 expect(res.body).toMatchObject({
                     name: 'IWA',
-                    grade: 105
+                    grade: 106
                 });
             })
             .end(done);
